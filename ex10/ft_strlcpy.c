6306438ad539c1bf4983135b7ddd5e01b253cb63
Rinor Ramli <40174427+pawaka2020@users.noreply.github.com>
2021-07-02 13:35:45 +0000

    ready for guacamole

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -1,4 +1,4 @@
-#include <string.h>
+//#//include <string.h>
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
@@ -7,10 +7,19 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	i = 0;
 	while (src[i])
 	{
-		if (i < size -1)
+		if (i < size)
 			dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (i);
 }
+
+// #include <stdio.h>
+// int main(void){
+//   char dest[] = "Hello";
+//   char src[] = "World";
+//   unsigned int size  = 1;
+//   printf("dest = %s, sec = %s, size = %d\n", dest, src, size);
+//   ft_strlcpy(dest, src, size);
+//   printf("After ft dest now = %s\n", dest);
+// }
